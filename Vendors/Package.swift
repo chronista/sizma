9fe4d6971dd72b01f7c5cd798744f52164f0e288
まみお <thesynsize@gmail.com>
2017-06-27 23:36:24 +0900

    不要ファイルの削除

--- Vendors/Package.swift
+++
@@ -1,6 +0,0 @@
-// swift-tools-version:3.1
-import PackageDescription
-let package = Package(name: "Vendors",targets: [],dependencies: [
-    .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(1, 0, 0)..<Version(3, .max, .max)),
-    .Package(url: "https://github.com/typelift/Swiftz", versions: Version(0,6,0)..<Version(1,0,0))
-])
